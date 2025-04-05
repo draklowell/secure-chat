f2cda911c353bb10632c79284eac50b310ac4d39
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2025-04-05 14:11:22 +0300

    Add docstrings for crypto.sha

--- crypto/sha.py
+++ crypto/sha.py
@@ -2,8 +2,24 @@ import hashlib
 
 
 class SHA256:
+    """
+    SHA256 hash algorithm.
+    Works as an adapter for the hashlib library.
+
+    Constants:
+        HASH_SIZE: Size of the hash in bytes.
+    """
     HASH_SIZE = 32
 
     @staticmethod
     def hash(data: bytes) -> bytes:
+        """
+        Hashes the given data using SHA256.
+
+        Args:
+            data: The data to hash.
+
+        Returns:
+            The SHA256 hash of the data.
+        """
         return hashlib.sha256(data).digest()
